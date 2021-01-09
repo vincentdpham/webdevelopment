b6326bc4426a481f53fa899791ebb8dd843322d4
Vincent <vincentdpham@gmail.com>
2021-01-08 23:57:13 -0800

    Completed API

--- 10 - Dad Jokes/script.js
+++ 10 - Dad Jokes/script.js
@@ -5,17 +5,31 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke(){
+async function generateJoke(){
     const config = {
         headers: {
-            'Accept': 'application/json'
+            Accept: 'application/json'
         },
     }
 
-    fetch('https://icanhazdadjoke.com', config)
-        .then((res) => res.json())
-        //.then((data) => console.log(data))
-        .then((data) => {
-            jokeEl.innerHTML = data.joke
-        })
+    const res = await fetch('https://icanhazdadjoke.com', config) // Because fetch is async, need to wait for it to be done first, so use await/async.
+    
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
 }
+
+// function generateJoke(){
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then((res) => res.json())
+//         //.then((data) => console.log(data))
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         })
+// }
