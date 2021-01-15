d48ddbcc4cd55109d8f9f44b13c36d8c1db6583e
Vincent <vincentdpham@gmail.com>
2021-01-14 16:32:55 -0800

    Nav Animation Complete

---
+++ 14 - Animated Nav/script.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+//toggle.addEventListener('click', function() {
+
+/*
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active')
+})
+
+*/
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active')
+})
